6c7ad000f21b16494b768626af48e0a7e9079402
Стрельников Сергей <kojes@yandex.ru>
2018-12-07 16:14:47 +0300

    defect filter adddata

--- ClientApp/components/defect-mainvue.vue
+++ ClientApp/components/defect-mainvue.vue
@@ -2,48 +2,69 @@
   <v-app>
     <div class="container w-100">
       <div class="row">
-        <div class="col-12">
+        <div class="col-6">
           <v-autocomplete v-model="selectedWafer"
                           :items="wafers"
-                          :label="`Выберите пластину для просмотра дефектов`"></v-autocomplete>
+                          :label="`Выберите пластину для просмотра дефектов`">
+
+          </v-autocomplete>
+        </div>
+        <div class="col-3">
+          <v-autocomplete v-model="selectedDie"
+                          :items="defectFilter.avbDiesList"
+                          :label="`Выберите кристалл`"
+                          chips
+                          no-data-text="Нет данных"
+                          item-text="code"
+                          item-value="dieId">
+
+          </v-autocomplete>
         </div>
       </div>
       <div class="row">
+        <loading :active.sync="filterLoading"
+                 :can-cancel="false"
+                 color="#4189C7"
+                 loader="bars"
+                 :is-full-page="false">
+
+        </loading>
+        
         <div class="col-3">
-          <!--<v-autocomplete v-model="model"
-                          :hint="!isEditing ? 'Click the icon to edit' : 'Click the icon to save'"
-                          :items="states"
-                          :readonly="!isEditing"
-                          :label="`State — ${isEditing ? 'Editable' : 'Readonly'}`"
-                          persistent-hint
-                          prepend-icon="mdi-city"></v-autocomplete>-->
+          <v-select v-model="selectedStages"
+                    :items="defectFilter.avbStagesList"
+                    chips
+                    item-text="stageName"
+                    no-data-text = "Нет данных"
+                    item-value="stageId"
+                    label="Выберите тех.этап"
+                    multiple>
+
+          </v-select>
         </div>
         <div class="col-3">
-          <!--<v-select :items="states"
-                    v-model="e7"
-                    label="Select"
-                    multiple
+          <v-select v-model="selectedDefectType"
+                    :items="defectFilter.avbDefectTypesList"
                     chips
-                    hint="What are the target regions"
-                    persistent-hint></v-select>-->
+                    no-data-text = "Нет данных"
+                    item-text="description"
+                    item-value="defectTypeId"
+                    label="Выберите тип дефекта"
+                    multiple>
+
+          </v-select>
         </div>
         <div class="col-3">
-          <!--<v-select :items="states"
-                    v-model="e7"
-                    label="Select"
-                    multiple
+          <v-select v-model="selectedDangerLevel"
+                    :items="defectFilter.avbDangerLevelList"
                     chips
-                    hint="What are the target regions"
-                    persistent-hint></v-select>-->
-        </div>
-        <div class="col-3">
-          <!--<v-select :items="states"
-                    v-model="e7"
-                    label="Select"
-                    multiple
-                    chips
-                    hint="What are the target regions"
-                    persistent-hint></v-select>-->
+                    no-data-text = "Нет данных"
+                    item-text="specification"
+                    item-value="dangerLevelId"
+                    label="Выберите опасность дефекта"
+                    multiple>
+
+          </v-select>
         </div>
       </div>
 
@@ -55,10 +76,12 @@
 
 <script>
   import DefectCard from './defect-card.vue'
-  
+  import Loading from 'vue-loading-overlay';
+  import 'vue-loading-overlay/dist/vue-loading.css';
+
   export default {
     components: {
-      'defect-card': DefectCard
+      'defect-card': DefectCard, Loading
     },
 
     created()
@@ -76,6 +99,7 @@
           this.$http.get(`/api/defect/getbywaferid?waferId=${selectedWafer}`).then((response) => {
             this.defects = response.data;
             let defects = this.defects;
+            this.filterLoading = true;
             this.$http.get('/api/defectfilter/GetDefectFilter', {
               params: {
                 defects: JSON.stringify(this.defects)
@@ -86,6 +110,7 @@
                   this.defectFilter = response.data.body;
                 }
                 else {
+                  this.defectFilter = [];
                   this.$swal({
                     type: response.data.responseType,
                     text: 'Дефектов на пластине не найдено',
@@ -95,10 +120,17 @@
                     timer: 4000
                   });
                 }
-
+                this.filterLoading = false;
               });
           });
+
+         
         
+      },
+
+      selectedDie: function ()
+      {
+
       }
 
       
@@ -109,10 +141,17 @@
 
     data() {
       return {
+        filterLoading: false,
         selectedWafer: null,
+        selectedDie: null,
+        selectedStages: null,
+        selectedDefectType: null,
+        selectedDangerLevel: null,
         wafers: [],
         defects: [],
-        defectFilter: {}
+        defectFilter: {},
+        dies: []
+
       }
     }
   }
