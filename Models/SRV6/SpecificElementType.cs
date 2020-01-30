db4a6bb67b7fca19cd761e4410af2a182aa94751
Сергей Стрельников <kojes@yandex.ru>
2020-01-30 10:40:35 +0300

    dbset specifictype

---
+++ Models/SRV6/SpecificElementType.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VueExample.Models.SRV6
+{
+    [Table("SpecificElementType")]
+    public class SpecificElementType
+    {
+        [Key]         
+        [Column("id_set")]
+        public int Id { get; set; }         
+        [Column("id_et")]
+        public int ElementType { get; set; }         
+        [Column("Name")]
+        public string Name { get; set; }
+        [Column("Specification")]
+        public string Specification { get; set; }
+    }
+}
