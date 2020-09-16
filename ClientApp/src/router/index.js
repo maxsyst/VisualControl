69122f004b40d08ae9f8b90a8aa105ee775c07f9
Сергей Стрельников <kojes@yandex.ru>
2020-09-16 14:49:22 +0300

    index html redirect

--- ClientApp/src/router/index.js
+++ ClientApp/src/router/index.js
@@ -7,7 +7,10 @@ Vue.use(VueRouter)
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: [{
+    path: '/index.html',
+    redirect: '/'
+  }, ...routes]
 })
 
 
